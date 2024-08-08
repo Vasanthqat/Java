2e6c7c827d82fec1dcdb974bb4d2f038474b4166
Vasanthqat <techpitstop@gmail.com>
2024-08-08 15:43:58 +0530

    Arithmatic operations code is added

--- src/Arithmatic/Operations.java
+++ src/Arithmatic/Operations.java
@@ -4,6 +4,10 @@ public class Operations {
 	public static void main(String[] args)
 	{
 		System.out.println("Sample calculator");
+		System.out.println(3+6);
+		System.out.println(3-6);
+		System.out.println(3*6);
+		System.out.println(3/6);
 	}
 
 }
